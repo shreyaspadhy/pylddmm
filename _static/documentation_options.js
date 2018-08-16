25e09bfa72346336a23ac4149a2febe7152f0cd1
Travis CI User <travis@example.org>
2018-08-16 04:20:34 +0000

    Update docs after building Travis build 4 of shreyaspadhy/pylddmm
    The docs were built from the branch 'master' against the commit
    e2b7c31017ee85de742adf67f7d4e61914ef8b74.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/shreyaspadhy/pylddmm/jobs/416652820.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.3/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post5+g303a7da',
+    VERSION: '0.post6+ge2b7c31',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
