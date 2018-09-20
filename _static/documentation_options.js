6ed8de1aa4f4e460a06f10aa7e209167b5cfab53
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2018-09-20 19:44:37 +0000

    Update docs after building Travis build 10 of shreyaspadhy/pylddmm
    The docs were built from the branch 'master' against the commit
    e54382773ec7fc98920d035ac863b0e50a07f316.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/shreyaspadhy/pylddmm/jobs/431192444.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.3/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post14+g7673d7d',
+    VERSION: '0.post15+ge543827',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
